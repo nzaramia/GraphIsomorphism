79f594b1d373cd72d6320ff1580098e7baad3aa6
nzaramia <77497748+nzaramia@users.noreply.github.com>
2022-02-10 21:16:11 -0500

    Add files via upload

--- GraphGenerator/GraphGenerator.py
+++ GraphGenerator/GraphGenerator.py
@@ -5,12 +5,21 @@ from PyQt5 import QtCore
 
 import random
 
+#NEXT TODO:
+#Setup the UI
 #TODO: random will have to be seeded so I could reproduce issues.
+#Generate isomorphisms
 
 class vertexDesc:
    def __init__(self, vertexId):
       self.vertexId = vertexId
       self.vertexNeighbors = []
+
+class edgeDesc:
+   def __init__(self, vertexIdStart, vertexIdEnd):
+      self.vertexIdStart = vertexIdStart
+      self.vertexIdEnd = vertexIdEnd
+
    
 
 class MainWindow(QWidget):
@@ -29,7 +38,7 @@ class MainWindow(QWidget):
       #Alse, minimum degree should be 1
 
       numNodes = 10
-      Degree = 4
+      Degree = 9
       fileName = "NZ-1"
       self.generateGraph(fileName, numNodes, Degree)
       
@@ -53,11 +62,13 @@ class MainWindow(QWidget):
       #Add the edges by adding nodes to the lists in vertDesc objects in the vertlist list
       
       generatedGraph = []
+      graphEdgeList = []
+      
       
       while len(vertList)>0:
                   
          #if there are more vertices left in the list than the degree of this vertex + 1 and we haven't reached the degree
-         while (len(vertList[0].vertexNeighbors)+1 < len(vertList)) and (len(vertList[0].vertexNeighbors) < requestedDegree):
+         while (len(vertList[0].vertexNeighbors) < requestedDegree):
             #get a random vertex index
             #add an edge between this and other vertex if no edge exists between them.  
             #if yes, choose another until we find one.            
@@ -66,6 +77,9 @@ class MainWindow(QWidget):
                if not vertList[vertexIndex].vertexId in vertList[0].vertexNeighbors:
                   vertList[0].vertexNeighbors.append(vertList[vertexIndex].vertexId)
                   vertList[vertexIndex].vertexNeighbors.append(vertList[0].vertexId)
+
+                  graphEdgeList.append(edgeDesc(vertList[0].vertexId, vertList[vertexIndex].vertexId))
+
                   #REMOVE THE NEIGHBOR FROM THE LIST IF REACHED DEGREE AND PUT IN GRAPH.
                   if len(vertList[vertexIndex].vertexNeighbors) == requestedDegree:
                      generatedGraph.append(vertList[vertexIndex])
@@ -81,22 +95,19 @@ class MainWindow(QWidget):
                graphVertexIndex = random.randint(0, len(generatedGraph)-1)
                vertList[0].vertexNeighbors.append(generatedGraph[graphVertexIndex].vertexId)
                generatedGraph[graphVertexIndex].vertexNeighbors.append(vertList[0].vertexId)
-               vertList.pop(0)
-         
-                          
-      
-      #!!!NEXT: TODO: take the processed list 
+
+               graphEdgeList.append(edgeDesc(vertList[0].vertexId, generatedGraph[graphVertexIndex].vertexId))
+
+         generatedGraph.append(vertList[0])      
+         vertList.pop(0)
+                                         
+      #write the graph into a file
       graphfile  = open(fileName, "w") 
       
-      graphfile.write("p edge " + str(numNodes) + " " + "???\n")  #TODO I don't know the number of edges yet.
+      graphfile.write("p edge " + str(numNodes) + " " + str(len(graphEdgeList)) + "\n")
 
-      #for e in range()   #need number of edges here
-      graphfile.write("e 1 4\n")
-      graphfile.write("e 1 5\n")     
-      graphfile.write("e 2 3\n")
-      graphfile.write("e 2 4\n")
-      graphfile.write("e 3 5\n")      
-      graphfile.write("e 4 5\n")
+      for e in graphEdgeList:
+         graphfile.write("e " + str(e.vertexIdStart) + ' ' + str(e.vertexIdEnd) + "\n")
       
       graphfile.close()
 
